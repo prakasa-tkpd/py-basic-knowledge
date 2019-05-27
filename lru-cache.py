8981c469b3c936fc1301796dda7a2bccc6f9f287
Prakasa TKPD <nedya.prakasa@tokopedia.com>
2019-05-27 15:28:51 +0700

    lru cache on function

---
+++ lru-cache.py
@@ -0,0 +1,15 @@
+import timeit
+import functools
+
+@functools.lru_cache(maxsize=128)
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+for x in range(100):
+    print(timeit.timeit('fibonacci(35)', globals=globals(), number=1))
+
+print(fibonacci.cache_info())
